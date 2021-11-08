09c73637df4598b3f4107920cb7f16d31532c85e
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-11-08 09:13:32 +0300

    Completed SignUp, commented out email requirement for testing simplicity

---
+++ apis/users/user.controllers.js
@@ -0,0 +1,29 @@
+const User = require("../../db/models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config/keys");
+
+const createToken = (user) => {
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+
+exports.signup = async (req, res, next) => {
+  try {
+    const saltrounds = 10;
+    const hashedPass = await bcrypt.hash(req.body.password, saltrounds);
+    req.body.password = hashedPass;
+
+    const newUser = await User.create(req.body);
+
+    createToken(newUser);
+
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
