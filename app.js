59eac41b668cc5496c4ce5a6457a1be04bc9c569
falafelpro <47983933+falafelpro@users.noreply.github.com>
2021-11-08 12:17:15 +0300

    Correction: user route path fixed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors());
 
 // Routes
 app.use("/media", express.static(path.join(__dirname, "media")));
-app.use("/apis/kk", userRoutes);
+app.use("/apis/users", userRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Path not found" });
